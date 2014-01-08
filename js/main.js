8919cfad336bedfa1c89bfd014c3dbd586c5077a
Kristen Gilden <kristen.gilden@gmail.com>
2014-01-08 23:17:30 +0200

    Fix simulation sometimes not starting when opening remotely

--- js/main.js
+++ js/main.js
@@ -6,15 +6,26 @@
 
 (function (document, kg, $) {
 
-    var svgObject,
-        context;
+    var svgObject;
 
     if (!(svgObject = document.getElementById('js-simulation'))) {
         throw 'No object with id "js-simulation" found.';
     }
 
-    kg.context = svgObject.contentDocument;
+    // Remotely opening the document requires using an event listener to
+    // detect, when the SVG is loaded. Turns out this doesn't work when
+    // opening locally.
+    if (document.location.hostname) {
+        // Source: http://stackoverflow.com/questions/2753732
+        svgObject.addEventListener('load', startSimulation, false);
+    } else {
+        startSimulation();
+    }
 
-    new kg.simulator();
+    function startSimulation() {
+        kg.context = svgObject.contentDocument;
+
+        new kg.simulator();
+    }
 
 })(document, kg, jQuery);
